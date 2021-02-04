69755cd8cc3bbb2b23ab9f7e7784c181199871b1
Nico Verbruggen <nico.verb@gmail.com>
2021-02-04 22:07:09 +0100

    🚚 Move files around

--- phpmon/Domain/Menu/StatsView.swift
+++ phpmon/Domain/Menu/StatsView.swift
@@ -32,15 +32,3 @@ class StatsView: NSView, XibLoadable {
         return item
     }
 }
-class HeaderView: NSView, XibLoadable {
-    @IBOutlet weak var textField: NSTextField!
-    
-    static func asMenuItem(text: String) -> NSMenuItem {
-        let view = Self.createFromXib()
-        view!.textField.stringValue = text.uppercased()
-        let item = NSMenuItem()
-        item.view = view
-        item.target = self
-        return item
-    }
-}
