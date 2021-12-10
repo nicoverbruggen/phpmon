498f4e7b79c724fb143fcacf8674e6927ad0562a
Nico Verbruggen <mail@nicoverbruggen.be>
2021-12-10 19:39:08 +0100

    👌 Polished context menu order and code

--- phpmon/Domain/SiteList/SiteListVC.swift
+++ phpmon/Domain/SiteList/SiteListVC.swift
@@ -261,67 +261,6 @@ class SiteListVC: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
     // MARK: - Context Menu
     
-    private func reloadContextMenu() {
-        let menu = NSMenu()
-        
-        guard let site = selectedSite else {
-            tableView.menu = nil
-            return
-        }
-        
-        if (site.aliasPath != nil) {
-            menu.addItem(
-                withTitle: "site_list.unlink".localized,
-                action: #selector(self.unlinkSite),
-                keyEquivalent: "U"
-            )
-            menu.addItem(NSMenuItem.separator())
-        }
-        
-        menu.addItem(
-            withTitle: site.secured
-            ? "site_list.unsecure".localized
-            : "site_list.secure".localized,
-            action: #selector(toggleSecure),
-            keyEquivalent: "L"
-        )
-        
-        if (applications.count > 0) {
-            menu.addItem(NSMenuItem.separator())
-            menu.addItem(withTitle: "site_list.detected_apps".localized, action: nil, keyEquivalent: "")
-            
-            for (index, editor) in applications.enumerated() {
-                let editorMenuItem = EditorMenuItem(
-                    title: "Open with \(editor.name)",
-                    action: #selector(self.openWithEditor(sender:)),
-                    keyEquivalent: "\(index + 1)"
-                )
-                editorMenuItem.editor = editor
-                menu.addItem(editorMenuItem)
-            }
-        }
-        
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(withTitle: "site_list.system_apps".localized, action: nil, keyEquivalent: "")
-        menu.addItem(
-            withTitle: "site_list.open_in_finder".localized,
-            action: #selector(self.openInFinder),
-            keyEquivalent: "F"
-        )
-        menu.addItem(
-            withTitle: "site_list.open_in_terminal".localized,
-            action: #selector(self.openInTerminal),
-            keyEquivalent: "T"
-        )
-        menu.addItem(
-            withTitle: "site_list.open_in_browser".localized,
-            action: #selector(self.openInBrowser),
-            keyEquivalent: "O"
-        )
-        
-        tableView.menu = menu
-    }
-    
     @objc func openWithEditor(sender: EditorMenuItem) {
         guard let editor = sender.editor else { return }
         editor.openDirectory(file: selectedSite!.absolutePath!)
