7a3dc9a145bb6c0f7f44ef4afe35de763ea5e6bb
Nico Verbruggen <mail@nicoverbruggen.be>
2021-12-20 18:31:16 +0100

    👌 Fix incorrect main.swift file

--- phpmon-cli/main.swift
+++ phpmon-cli/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+// First, let's read the initial command line argument
 
+print(CommandLine.arguments)
+
+if CommandLine.arguments.count != 3 {
+    print("You must enter two arguments.")
+    exit(1)
+}
+
+let argument = CommandLine.arguments[1]
+
+if !AllowedArguments.has(argument) {
+    print("The supported arguments are: \(AllowedArguments.rawValues)")
+    exit(1)
+}
+
+let action = AllowedArguments.init(rawValue: argument)
+
+switch action {
+case .use:
+    // Read the PHP value
+    let version = CommandLine.arguments[2]
+    print("Switching to PHP \(version)...")
+    break
+case .none:
+    print("Action not recognized!")
+    exit(1)
+}
