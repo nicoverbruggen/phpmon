ab3a735d977685fed96bd6777bff09208ca3f959
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-01-26 12:01:04 -0800

    :sparkles: Add Flarum to framework list
    Not 100% sure if this is fitting, since its not so much a "framework" - but I saw Wordpress on the list so I thought it might be alright 😄

--- phpmon/Domain/Integrations/Composer/PhpFrameworks.swift
+++ phpmon/Domain/Integrations/Composer/PhpFrameworks.swift
@@ -29,6 +29,7 @@ struct PhpFrameworks {
         
         "craftcms/craft": "Craft",
         "drupal/core": "Drupal",
+        "flarum/flarum": "Flarum",
         "tightenco/jigsaw": "Jigsaw",
         "joomla/uri": "Joomla",
         "themsaid/katana": "Katana",
