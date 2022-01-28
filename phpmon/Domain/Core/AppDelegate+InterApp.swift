510257c4363bfa4ea63baae85fffe2cd0a1885ac
Nico Verbruggen <mail@nicoverbruggen.be>
2022-01-28 22:05:53 +0100

    👌 Complete work on inter app handler
    Allowed commands:
    
    phpmon://list
    phpmon://services/stop
    phpmon://services/restart/all
    phpmon://services/restart/nginx
    phpmon://services/restart/php
    phpmon://services/restart/dnsmasq
    phpmon://locate/config
    phpmon://locate/composer
    phpmon://locate/valet
    phpmon://phpinfo
    phpmon://switch/php/{version}

--- phpmon/Domain/Core/AppDelegate+InterApp.swift
+++ phpmon/Domain/Core/AppDelegate+InterApp.swift
@@ -21,44 +21,22 @@ extension AppDelegate {
      Please note that PHP Monitor needs to be running in the background for this to work.
      */
     func application(_ application: NSApplication, open urls: [URL]) {
-        // Only ever interpret the first URL
-        if let url = urls.first {
-            self.interpretCommand(
-                url.absoluteString.replacingOccurrences(of: "phpmon://", with: "")
-            )
-        }
+        
+        guard let url = urls.first else { return }
+        
+        self.interpretCommand(
+            url.absoluteString.replacingOccurrences(of: "phpmon://", with: ""),
+            commands: InterApp.getCommands()
+        )
     }
     
-    private func interpretCommand(_ command: String) {
-        switch (command) {
-        case "list":
-            SiteListVC.show()
-            break
-        default:
-            break
-        }
-        
-        if command.starts(with: "callback/switch") {
-            // TODO: Send XML for suggestions?
-            // I looked at the output of Dash (which is my example of an amazing app here)
-            // and it works like this: open -g "dash-workflow-callback://{query}"
-            // which returns XML. I'm not sure how to do that here, but if I could
-            // dynamically return a list of valid PHP versions, it'd be easy mode.
-        }
-        
-        if command.starts(with: "switch/php/") {
-            // See if the PHP version we're attempting to switch to is valid
-            let version = String(command.split(separator: "/").last!)
-            if PhpEnv.shared.availablePhpVersions.contains(version) {
-                MainMenu.shared.switchToPhpVersion(version)
-            } else {
-                Alert.notify(
-                    message: "Unsupported version",
-                    info: "PHP Monitor can't switch to PHP \(version), as it may not be installed or available."
-                )
+    private func interpretCommand(_ command: String, commands: [InterApp.Action]) {
+        commands.forEach { action in
+            if command.starts(with: action.command) {
+                let lastElement = String(command.split(separator: "/").last!)
+                action.action(lastElement)
             }
         }
     }
-    
 }
 
