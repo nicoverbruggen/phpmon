ffb112cfb23886aea06622f1cbef1d5f94f815a9
Nico Verbruggen <mail@nicoverbruggen.be>
2022-02-08 21:26:06 +0100

    🚛 Reorganise Errors structs

--- phpmon/Common/Errors/Errors.swift
+++ phpmon/Common/Errors/Errors.swift
@@ -8,4 +8,22 @@
 
 import Foundation
 
+// MARK: - Alertable Errors
+// These errors must be resolved by the user.
+
+struct HomebrewPermissionError: Error, AlertableError {
+    enum Kind: String {
+        case applescriptNilError = "homebrew_permissions.applescript_returned_nil"
+    }
+    
+    let kind: Kind
+    
+    func getErrorMessageKey() -> String {
+        return "alert.errors.\(self.kind.rawValue)"
+    }
+}
+
+// MARK: - Errors that do not have an associated alert message
+// The errors must be resolved by the developer.
+
 struct VersionParseError: Error {}
