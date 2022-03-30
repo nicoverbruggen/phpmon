d3657716c4f2c20fa9424e1f3b913006e48cc8fc
Nico Verbruggen <mail@nicoverbruggen.be>
2022-03-30 21:26:53 +0200

    ♻️ Added `ValetProxy` struct

---
+++ phpmon/Domain/Integrations/Valet/ValetProxy.swift
@@ -0,0 +1,18 @@
+//
+//  ValetProxy.swift
+//  PHP Monitor
+//
+//  Created by Nico Verbruggen on 30/03/2022.
+//  Copyright © 2022 Nico Verbruggen. All rights reserved.
+//
+
+import Foundation
+
+struct ValetProxy
+{
+    var domain: String
+    
+    var tld: String
+    
+    var target: String
+}
