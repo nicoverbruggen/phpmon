f098ffbf3d28f5192f06b3f3b7fa97f7d4907ef6
Nico Verbruggen <mail@nicoverbruggen.be>
2022-07-26 19:30:35 +0200

    ♻️ Consistency in naming of window controllers

---
+++ phpmon/Domain/Progress/ProgressVC.swift
@@ -0,0 +1,24 @@
+//
+//  ProgressVC.swift
+//  PHP Monitor
+//
+//  Created by Nico Verbruggen on 26/07/2022.
+//  Copyright © 2022 Nico Verbruggen. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ProgressViewController: NSViewController {
+
+    @IBOutlet weak var labelTitle: NSTextField!
+    @IBOutlet weak var labelDescription: NSTextField!
+
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var imageViewType: NSImageView!
+
+    deinit {
+        Log.perf("Deinitializing ProgressViewController")
+    }
+
+}
