8d46fa8a4eb69f9d0ee048370f44e457da35ab91
Nico Verbruggen <mail@nicoverbruggen.be>
2022-09-05 20:05:16 +0200

    👌 Right-clicking a row selects it in domain list

---
+++ phpmon/Domain/DomainList/PMTableView.swift
@@ -0,0 +1,27 @@
+//
+//  PMTableView.swift
+//  PHP Monitor
+//
+//  Created by Nico Verbruggen on 05/09/2022.
+//  Copyright © 2022 Nico Verbruggen. All rights reserved.
+//
+
+import Cocoa
+
+/**
+ This subclassed version of NSTableView selects a row upon right-clicking,
+ thus making the domain list behave more like you'd expect.
+ */
+public class PMTableView: NSTableView {
+
+    override open func menu(for event: NSEvent) -> NSMenu? {
+        let row = self.row(at: self.convert(event.locationInWindow, from: nil))
+
+        if row >= 0 {
+            self.selectRowIndexes(IndexSet(integer: row), byExtendingSelection: false)
+        }
+
+        return super.menu(for: event)
+    }
+
+}
