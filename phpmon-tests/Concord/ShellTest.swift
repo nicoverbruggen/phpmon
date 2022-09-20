3f25759d4fdbd497bb7e8d591c534d46089cc916
Nico Verbruggen <mail@nicoverbruggen.be>
2022-09-20 20:49:29 +0200

    🏗 Fix SwiftLint, WIP shell rework

---
+++ phpmon-tests/Concord/ShellTest.swift
@@ -0,0 +1,32 @@
+//
+//  ShellTest.swift
+//  phpmon-tests
+//
+//  Created by Nico Verbruggen on 20/09/2022.
+//  Copyright © 2022 Nico Verbruggen. All rights reserved.
+//
+
+import XCTest
+
+class ShellTest: XCTestCase {
+    func test_default_shell_is_system_shell() {
+        XCTAssertTrue(NewShell.shared is SystemShell)
+
+        XCTAssertTrue(NewShell.shared.syncPipe("php -v")
+            .contains("Copyright (c) The PHP Group"))
+
+        let expectedPhpOutput = """
+                PHP 8.1.10 (cli) (built: Sep  3 2022 12:09:27) (NTS)
+                Copyright (c) The PHP Group
+                Zend Engine v4.1.10, Copyright (c) Zend Technologies
+                with Zend OPcache v8.1.10, Copyright (c), by Zend Technologies
+                with Xdebug v3.1.4, Copyright (c) 2002-2022, by Derick Rethans
+            """
+
+        NewShell.useTestable([
+            "php -v": expectedPhpOutput
+        ])
+
+        XCTAssertEqual(expectedPhpOutput, NewShell.shared.syncPipe("php -v"))
+    }
+}
--- phpmon/Common/Core/NewShell.swift
+++ phpmon/Common/Core/NewShell.swift
@@ -9,20 +9,64 @@
 import Foundation
 
 class NewShell {
-    static var shared: Shellable!
+    static var shared: Shellable = SystemShell()
 
-    public func useTestable(_ expectations: [String: String]) {
+    /// Uses a testable shell with predefined responses. You specify the terminal's output.
+    public static func useTestable(_ expectations: [String: String]) {
         Self.shared = TestableShell(expectations: expectations)
+    }
+
+    /// Reverts back to the system shell. You do not need to call this, only after using `useTestable()`.
+    public static func useSystem() {
+        Self.shared = SystemShell()
     }
 }
 
 protocol Shellable {
-    func pipe(_ command: String) -> String
+    func syncPipe(_ command: String) -> String
+    func pipe(_ command: String) async -> String
 }
 
 class SystemShell: Shellable {
-    func pipe(_ command: String) -> String {
-        return "shell output"
+    public var launchPath: String = "/bin/sh"
+
+    public var exports: String = ""
+
+    private func getShellProcess(for command: String) -> Process {
+        var completeCommand = ""
+
+        // Basic export (PATH)
+        completeCommand += "export PATH=\(Paths.binPath):$PATH && "
+
+        // Put additional exports in between
+        if !self.exports.isEmpty {
+            completeCommand += "\(self.exports) && "
+        }
+
+        completeCommand += command
+
+        let task = Process()
+        task.launchPath = self.launchPath
+        task.arguments = ["--noprofile", "-norc", "--login", "-c", completeCommand]
+        return task
+    }
+
+    func syncPipe(_ command: String) -> String {
+        let task = getShellProcess(for: command)
+        let pipe = Pipe()
+
+        task.standardOutput = pipe
+        task.launch()
+
+        return String(
+            data: pipe.fileHandleForReading.readDataToEndOfFile(),
+            encoding: .utf8
+        ) ?? ""
+    }
+
+    func pipe(_ command: String) async -> String {
+        // TODO
+        return ""
     }
 }
 
@@ -33,7 +77,11 @@ class TestableShell: Shellable {
 
     var expectations: [String: String] = [:]
 
-    func pipe(_ command: String) -> String {
+    func pipe(_ command: String) async -> String {
+        return expectations[command] ?? ""
+    }
+
+    func syncPipe(_ command: String) -> String {
         return expectations[command] ?? ""
     }
 }
