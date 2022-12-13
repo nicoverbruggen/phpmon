a696a9a38683a8544cb45389789fd1a02fcf04c3
Nico Verbruggen <mail@nicoverbruggen.be>
2022-12-13 23:32:50 +0100

    👌 Style fix

--- phpmon/Domain/App/App.swift
+++ phpmon/Domain/App/App.swift
@@ -42,7 +42,7 @@ class App {
 
         var systeminfo = utsname()
         uname(&systeminfo)
-        let machine = withUnsafeBytes(of: &systeminfo.machine) {bufPtr->String in
+        let machine = withUnsafeBytes(of: &systeminfo.machine) { bufPtr -> String in
             let data = Data(bufPtr)
             if let lastIndex = data.lastIndex(where: {$0 != 0}) {
                 return String(data: data[0...lastIndex], encoding: .isoLatin1)!
