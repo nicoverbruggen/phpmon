291d20b2b30e7c12a74586f014d6e7d177962b0a
Nico Verbruggen <mail@nicoverbruggen.be>
2023-01-06 12:59:15 +0100

    🏗 WIP: Functional services list

--- phpmon/Domain/App/Services/ValetServicesManager.swift
+++ phpmon/Domain/App/Services/ValetServicesManager.swift
@@ -17,54 +17,50 @@ class ValetServicesManager: ServicesManager {
     }
 
     override func reloadServicesStatus() async {
-        // TODO
+        await withTaskGroup(of: [HomebrewService].self, body: { group in
+            group.addTask {
+                let rootServiceNames = self.formulae
+                    .filter { $0.elevated }
+                    .map { $0.name }
+
+                let rootJson = await Shell
+                    .pipe("sudo \(Paths.brew) services info --all --json")
+                    .out.data(using: .utf8)!
+
+                return try! JSONDecoder()
+                    .decode([HomebrewService].self, from: rootJson)
+                    .filter({ return rootServiceNames.contains($0.name) })
+            }
+
+            group.addTask {
+                let userServiceNames = self.formulae
+                    .filter { !$0.elevated }
+                    .map { $0.name }
+
+                let normalJson = await Shell
+                    .pipe("\(Paths.brew) services info --all --json")
+                    .out.data(using: .utf8)!
+
+                return try! JSONDecoder()
+                    .decode([HomebrewService].self, from: normalJson)
+                    .filter({ return userServiceNames.contains($0.name) })
+            }
+
+            for await services in group {
+                for service in services {
+                    guard let wrapper = self[service.name] else {
+                        break
+                    }
+
+                    wrapper.service = service
+                }
+            }
+
+            for wrapper in serviceWrappers {
+                wrapper.isBusy = false
+            }
+
+            self.broadcastServicesUpdated()
+        })
     }
 }
-
-// TODO
-
-/*
-public static func loadHomebrewServices() async {
-    await Self.shared.updateServicesList()
-
-    Task {
-        let rootServiceNames = Self.shared.formulae
-            .filter { $0.elevated }
-            .map { $0.name }
-
-        let rootJson = await Shell
-            .pipe("sudo \(Paths.brew) services info --all --json")
-            .out.data(using: .utf8)!
-
-        let rootServices = try! JSONDecoder()
-            .decode([HomebrewService].self, from: rootJson)
-            .filter({ return rootServiceNames.contains($0.name) })
-
-        Task { @MainActor in
-            for service in rootServices {
-                Self.shared.services[service.name]!.service = service
-            }
-        }
-    }
-
-    Task {
-        let userServiceNames = Self.shared.formulae
-            .filter { !$0.elevated }
-            .map { $0.name }
-
-        let normalJson = await Shell
-            .pipe("\(Paths.brew) services info --all --json")
-            .out.data(using: .utf8)!
-
-        let userServices = try! JSONDecoder()
-            .decode([HomebrewService].self, from: normalJson)
-            .filter({ return userServiceNames.contains($0.name) })
-
-        Task { @MainActor in
-            for service in userServices {
-                Self.shared.services[service.name]!.service = service
-            }
-        }
-    }
-}
-*/
