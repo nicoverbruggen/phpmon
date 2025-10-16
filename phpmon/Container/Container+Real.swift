8eca1a55b5a230d1d33f64fe53bdcd4b97e348ca
Nico Verbruggen <mail@nicoverbruggen.be>
2025-10-16 19:01:55 +0200

    ♻️ Container refactoring

---
+++ phpmon/Container/Container+Real.swift
@@ -0,0 +1,15 @@
+//
+//  Container+Real.swift
+//  PHP Monitor
+//
+//  Created by Nico Verbruggen on 16/10/2025.
+//  Copyright © 2025 Nico Verbruggen. All rights reserved.
+//
+
+extension Container {
+    public static func real() -> Container {
+        let container = Container()
+        container.prepare()
+        return container
+    }
+}
--- phpmon/Container/Container.swift
+++ phpmon/Container/Container.swift
@@ -12,37 +12,35 @@ class Container {
     var filesystem: FileSystemProtocol!
     var command: CommandProtocol!
 
-    // Additional abstractions
+    // Extra abstractions
     var paths: Paths!
     var phpEnvs: PhpEnvironments!
     var favorites: Favorites!
+    var warningManager: WarningManager! // pending rename?
 
-    // TODO: Pending a rename
-    var warningManager: WarningManager!
-
+    ///
+    /// The initializer is empty. You must call `prepare` to enable the container.
+    /// To avoid issues with unsafe access, the actual objects are set in `prepare`.
+    /// `self` is not available in this constructor, after all. The alternative
+    /// is to use lazy variables here, but I don't think it's that clean, especially
+    /// given the other initializers available via the extensions.
+    ///
     init() {}
 
+    ///
+    /// Creates new instances belonging to the container, while referencing
+    /// the container itself and passing the reference on to each component that needs it.
+    ///
     public func prepare() {
+        // Core
         self.shell = RealShell(container: self)
         self.filesystem = RealFileSystem(container: self)
         self.command = RealCommand()
 
+        // Extra
         self.paths = Paths(container: self)
         self.phpEnvs = PhpEnvironments(container: self)
-
         self.favorites = Favorites()
         self.warningManager = WarningManager(container: self)
-    }
-
-    public func overrideWith(config: TestableConfiguration) {
-        self.shell = TestableShell(expectations: config.shellOutput)
-        self.filesystem = TestableFileSystem(files: config.filesystem)
-        self.command = TestableCommand(commands: config.commandOutput)
-    }
-
-    public static func real() -> Container {
-        let container = Container()
-        container.prepare()
-        return container
     }
 }
