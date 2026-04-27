d7e27ca400c3d86ed28b9fd7727dfcc5f7e8c367
Nico Verbruggen <mail@nicoverbruggen.be>
2026-04-27 12:56:12 +0200

    ♻️ WIP: PhpHelper refactor

---
+++ phpmon/Common/PHP/PHP Helper/PhpHelperTemplates.swift
@@ -0,0 +1,12 @@
+//
+//  PhpHelperTemplates.swift
+//  PHP Monitor
+//
+//  Created by Nico Verbruggen on 27/04/2026.
+//  Copyright © 2026 Nico Verbruggen. All rights reserved.
+//
+
+struct PhpHelperTemplates {
+    // For three supported shells: bash, zsh, fish
+    // If possible, a single syntax is preferred?
+}
